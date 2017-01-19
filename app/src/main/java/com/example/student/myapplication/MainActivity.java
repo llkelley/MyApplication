07740421137907018acd1699df73bff00439f505
Lori Rodas <llkelley@madisoncollege.edu>
2017-01-18 19:36:14 -0600

    Initial commit

---
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.student.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TODO wireup widgets
+
+        //TODO add listeners
+    }
+}
